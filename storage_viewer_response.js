df63be78825bdc94915645106940dd1b84d728a5
Ben Naylor <nayben@gmail.com>
2023-03-22 13:06:27 +0100

    Enable remapping of many different headers
    We will need some extra headers for dealing with docker specific
    requirements as per https://docs.docker.com/registry/spec/api/#api-version-check

--- storage_viewer_response.js
+++ storage_viewer_response.js
@@ -1,11 +1,28 @@
+// This is a hash map of S3 header values to map to the response header values
+var HEADER_MAPPINGS = {
+  'x-amz-meta-docker-etag': 'etag',
+  'x-amz-meta-docker-distribution-api-version': 'Docker-Distribution-API-Version',
+}
+
+// This function returns a curried function which edits `response`. The curried
+// function should only accept an array of 2 values, however cloudfront
+// functions are limited in to their usage of ES6+ features.
+function mapHeadersToResponse(response) {
+  return function(mapping) {
+    var header = mapping[0];
+    var newHeader = mapping[1];
+    if (response.headers[header] && response.headers[header]['value']) {
+      response.headers[newHeader] = { value: response.headers[header]['value'] }
+    }
+  }
+}
+
 function handler(event) {
   var response = event.response;
 
-  if (response.headers['x-amz-meta-docker-etag'] && response.headers['x-amz-meta-docker-etag']['value']) {
-    response.headers['etag'] = {
-      value: response.headers['x-amz-meta-docker-etag']['value']
-    };
-  }
+  // map the returned headers to other headers in the response object
+  var headerMapper = mapHeadersToResponse(response);
+  Object.entries(HEADER_MAPPINGS).forEach(headerMapper)
 
   return response;
 }
