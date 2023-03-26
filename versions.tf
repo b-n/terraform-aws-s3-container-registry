0f0b5601e1e7310470e0bb2bc399d983e80aec74
Ben Naylor <nayben@gmail.com>
2023-03-26 16:32:34 +0200

    Remove unused terraform moudle (hashicorp archive)

--- versions.tf
+++ versions.tf
@@ -4,11 +4,6 @@ terraform {
       source  = "hashicorp/aws"
       version = "4.59.0"
     }
-
-    archive = {
-      source  = "hashicorp/archive"
-      version = "2.3.0"
-    }
   }
 
   required_version = "~> 1.0"
